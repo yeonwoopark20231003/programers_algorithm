00f6fd1cb6951d35fbcb49586e474824ed6815f6
yeonwoopark20231003 <146754105+yeonwoopark20231003@users.noreply.github.com>
2024-05-02 17:19:55 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.c
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.c
@@ -2,20 +2,18 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-long long solution(int price, int money, int count) {
-    long answer = 0;
-        long total=0;
-		
-        //탄 횟수만큼 금액 늘리며 더하기
-        for(int i=1; i<count+1; i++){
-            total += price*i;
-        }
-        //가진 돈보다 비용이 많이 들 경우
-        if(money<total){
-            answer = total-money;
-        //모자라지 않는 경우
-        }else{
-            answer = 0;
-        }
-        return answer;
+long solution(int price, int money, int count) {
+    long answer = money;
+    
+    for(int i=1; i<=count; i++){
+        answer -= price*i;
     }
+    
+    if(answer<0){
+        answer = answer *-1;
+    } else{
+        answer =0;
+    }
+    
+    return answer;
+}
